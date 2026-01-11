7aab2106d3e2d42db57e0be294293b4cf73a949b
julkabar <julkabaryba@gmail.com>
2026-01-12 01:47:24 +0200

    КР [ок?]

---
+++ web/src/main/java/yyb/julkabar/web/HomeController.java
@@ -0,0 +1,13 @@
+package yyb.julkabar.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToBooks() {
+        return "redirect:/books";
+    }
+}
